dcf958e3024fd7de2aef7bd08972007b60edd248
Joas <94909163+pontonkid@users.noreply.github.com>
2024-07-08 12:23:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ import scipy.io.wavfile as wavfile
 import numpy as np
 
 
+
+caption_image = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+
+Narrator = pipeline("text-to-speech", model="kakao-enterprise/vits-ljs")
+
+
+
+
+
